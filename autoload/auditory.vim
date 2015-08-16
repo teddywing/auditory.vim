teddywing/auditory.vim

commit 71ec214cdb8814ecb71c837bf947583fda54d37e

Create a setting for Galaxy Far Far Away sounds
Adding a configuration variable for these sounds allows them to be
turned on by default without having to
`:AuditoryToggleGalaxyFarFarAway`, so if you prefer these sounds you can
enable them at the outset.

autoload/auditory.vim
@@ -169,20 +169,19 @@ function! s:GalaxyFarFarAway()
 endfunction
 
 
-let s:galaxy_far_far_away = 0
 function! auditory#ToggleGalaxyFarFarAway()
-	if s:galaxy_far_far_away
+	if g:auditory_galaxy_far_far_away
 		augroup auditory#insert_mode
 			autocmd!
 			autocmd CursorMovedI * call auditory#PlayScale()
 		augroup END
-		let s:galaxy_far_far_away = 0
+		let g:auditory_galaxy_far_far_away = 0
 	else
 		augroup auditory#insert_mode
 			autocmd!
 			autocmd CursorMovedI * call <SID>GalaxyFarFarAway()
 		augroup END
-		let s:galaxy_far_far_away = 1
+		let g:auditory_galaxy_far_far_away = 1
 	endif
 endfunction
 

plugin/auditory.vim
@@ -15,6 +15,10 @@ if !exists('g:auditory_on')
 	let g:auditory_on = 0
 endif
 
+if !exists('g:auditory_galaxy_far_far_away')
+	let g:auditory_galaxy_far_far_away = 0
+endif
+
 
 command! AuditoryOn call auditory#AssignMappings()
 command! AuditoryOff call auditory#Unmap()
@@ -24,4 +28,9 @@ command! AuditoryToggleGalaxyFarFarAway call auditory#ToggleGalaxyFarFarAway()
 
 if g:auditory_on
 	call auditory#AssignMappings()
+endif
+
+if g:auditory_galaxy_far_far_away
+	let g:auditory_galaxy_far_far_away = 0
+	call auditory#ToggleGalaxyFarFarAway()
 endif

commit 1c9fe852b40d4e7a883bcdc71c91abdd023514bb

Don't turn on Galaxy Far Far away when not `g:auditory_on`
My change from 71ec214cdb8814ecb71c837bf947583fda54d37e caused Galaxy
Far Far Away sounds to be turned on any time you had
`g:auditory_galaxy_far_far_away` turned on, regardless of whether
`g:auditory_on` was also turned on.

Fix this because we never want sounds to play when `g:auditory_on` is
off.

We now rely on `auditory#AssignInsertMappings()` to know whether to use
Galaxy Far Far Away or default insert mode sounds.

Need to toggle `g:auditory_galaxy_far_far_away` in that function because
we're calling the toggle function so we need to set it to the opposite
of what we want.

In the future if we add more insert mode sounds we'll have to change
this structure around but it should work for now.

autoload/auditory.vim
@@ -77,10 +77,8 @@ endfunction
 
 
 function! auditory#AssignInsertMappings()
-	augroup auditory#insert_mode
-		autocmd!
-		autocmd CursorMovedI * call auditory#PlayScale()
-	augroup END
+	let g:auditory_galaxy_far_far_away = !g:auditory_galaxy_far_far_away
+	call auditory#ToggleGalaxyFarFarAway()
 endfunction
 
 

plugin/auditory.vim
@@ -29,8 +29,3 @@ command! AuditoryToggleGalaxyFarFarAway call auditory#ToggleGalaxyFarFarAway()
 if g:auditory_on
 	call auditory#AssignMappings()
 endif
-
-if g:auditory_galaxy_far_far_away
-	let g:auditory_galaxy_far_far_away = 0
-	call auditory#ToggleGalaxyFarFarAway()
-endif